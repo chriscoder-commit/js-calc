a4fbaacc637403cc7809a3bcc44796dca046800a
Chris Scott <cam022007@coderacademy.edu.au>
2020-12-02 19:28:56 -0800

    made code dryer by putting maths on a separate page and exporting the functions

---
+++ maths.js
@@ -0,0 +1,38 @@
+// this will perform basic addition 
+const addition = (num1, num2) => {
+  return num1 + num2;
+};
+
+// this will perform basic subtraction
+const subtraction = (num1, num2) => {
+  return num1 - num2;
+};
+
+// this will perform basic multiplication
+const multiplication = (num1, num2) => {
+  return num1 * num2;
+};
+
+// this will perform basic division 
+const division = (num1, num2) => {
+  return num1 / num2;
+};
+
+// this will give you the square root of a number
+const squareRoot = (num) => {
+  return num * num;
+};
+
+// this will give you a number to the power of another number
+const powerOf = (num1, num2) => {
+  return num1 ** num2; 
+};
+
+module.exports = {
+  addition: addition,
+  subtraction: subtraction,
+  multiplication: multiplication,
+  division: division,
+  squareRoot: squareRoot,
+  powerOf: powerOf,
+}
--- script.js
+++ script.js
@@ -1,47 +1,21 @@
+const maths = require("./maths")
+
+const menu = () => {
 console.log('Welcome to the calculator, I am a basic calculator you can use!')
 console.log('What calculation would you like to perform?')
-
-
-// this will perform basic addition 
-const addition = (num1, num2) => {
-  return num1 + num2;
+console.log (`
+1: add 2 numbers
+2: subtract 2 numbers
+3: multiply 2 numbers
+4: divide 2 numbers
+5: square a number
+6: determine power of two numbers 
+`)
 };
 
-console.log(addition(3, 4))
+menu()
+console.log(maths.addition(3, 2))
 
-// this will perform basic subtraction
-const subtraction = (num1, num2) => {
-  return num1 - num2;
-};
 
-console.log(subtraction(100, 68))
-
-// this will perform basic multiplication
-const multiplication = (num1, num2) => {
-  return num1 * num2;
-};
-
-console.log(multiplication(4, 5))
-
-// this will perform basic division 
-const division = (num1, num2) => {
-  return num1 / num2;
-};
-
-console.log(division(20, 5));
-
-// this will give you the square root of a number
-const squareRoot = (num) => {
-  return num * num;
-};
-
-console.log(squareRoot(5))
-
-// this will give you a number to the power of another number
-const powerOf = (num1, num2) => {
-  return num1 ** num2; 
-};
-
-console.log(powerOf(3, 4))
 
 
